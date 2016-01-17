a4df447ef83e5fcf649de7bec0086f7b33ff3bc9
Oliver San Juan <osanju2@gmail.com>
2016-01-17 11:10:12 -0600

    Completed basic tip calculator

--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -7,17 +7,80 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    //MARK: Class Variables
+    
+    //Labels
+    @IBOutlet var tenLabel: UILabel!
+    @IBOutlet var displayTen: UILabel!
+    
+    @IBOutlet var fifteenLabel: UILabel!
+    @IBOutlet var displayFifteen: UILabel!
+    
+    @IBOutlet var twentyLabel: UILabel!
+    @IBOutlet var displayTwenty: UILabel!
+    
+    @IBOutlet var twentyFiveLabel: UILabel!
+    @IBOutlet var displayTwentyFive: UILabel!
+    
+    @IBOutlet var enteredAmt: UITextField!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Hide the labels on start
+        tenLabel.hidden = true
+        displayTen.hidden = true
+        
+        fifteenLabel.hidden = true
+        displayFifteen.hidden = true
+        
+        twentyLabel.hidden = true
+        displayTwenty.hidden = true
+        
+        twentyFiveLabel.hidden = true
+        displayTwentyFive.hidden = true
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func calcButton(sender: AnyObject) {
+        
+        //Calculate the tips
+        let amount = (enteredAmt.text! as NSString).floatValue
+        
+        //Display the labels
+        tenLabel.hidden = false
+        displayTen.hidden = false
+    
+        fifteenLabel.hidden = false
+        displayFifteen.hidden = false
+        
+        twentyLabel.hidden = false
+        displayTwenty.hidden = false
+        
+        twentyFiveLabel.hidden = false
+        displayTwentyFive.hidden = false
+        
+        
+        //Calculate and display
+        displayTen.text = String(format: "$%.2f", arguments: [amount * 0.10])
+        displayFifteen.text = String(format: "$%.2f", arguments: [amount * 0.15])
+        displayTwenty.text = String(format: "$%.2f", arguments: [amount * 0.20])
+        displayTwentyFive.text = String(format: "$%.2f", arguments: [amount * 0.25])
+        
+        
+        
     }
 
 
